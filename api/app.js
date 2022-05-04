6ae8b90bff662b26167191ead6a7a72a0bdfd4c9
AnttiKurkinen <antti.kurkinen01@gmail.com>
2022-05-04 12:11:46 +0300

    korjauksia

--- api/app.js
+++ api/app.js
@@ -1,3 +1,4 @@
+var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -32,6 +33,8 @@ var tilitapahtumatRouter = require('./routes/tilitapahtumat');
 
 var app = express();
 
+app.use(logger('dev'));
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
